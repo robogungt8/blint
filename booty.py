7b271952b5288c66d476e1fc8424fe487ebeb8c5
Bum Kim <buki4157@colorado.edu>
2017-04-23 01:11:22 -0600

    can find 'me too thanks' comments and reply

--- booty.py
+++ booty.py
@@ -5,13 +5,24 @@ import re
 
 reddit = praw.Reddit('wholesome_booty')
 
-subreddit = reddit.subreddit('renosubaru')
+subreddit = reddit.subreddit('me_irl')
+
 
 # subreddit.submit(
 #     'Colorado has more Subarus',
 #     'Title says all')
 
+
 for submission in subreddit.hot(limit=5):
-    if re.search("colorado", submission.title, re.IGNORECASE):
-        submission.reply("Size doesn't matter. It's about the passion.")
-        print "Bot replying to: " + submission.title
+    for top_level_comment in submission.comments:
+        print(top_level_comment.body)
+        if re.search("me too thanks", top_level_comment.body, re.IGNORECASE):
+            top_level_comment.reply("Me 3 thx <3")
+            print top_level_comment.body
+            # comments.append(tb(top_level_comment.body))
+            # print "We would reply to: " + top_level_comment.body
+
+
+    # if re.search("me too thanks", submission.title, re.IGNORECASE):
+    #     submission.reply("Size doesn't matter. It's about the passion.")
+    #     print "Bot replying to: " + submission.title
