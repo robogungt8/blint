d5e017e71bffea0385865b9626d8c6a647877975
Bum Kim <buki4157@colorado.edu>
2017-04-23 11:28:18 -0600

    I've made a terrible mistake

--- booty.py
+++ booty.py
@@ -8,7 +8,7 @@ import gensim
 import praw
 import re
 
-tokenizer = RegexpTokenizer(r'\w+')
+tokenizer = RegexpTokenizer(r'[\w\'\/]+')
 
 # create English stop words list
 en_stop = get_stop_words('en')
@@ -27,12 +27,13 @@ subreddit = reddit.subreddit('me_irl')
 
 comments = []
 
-for submission in subreddit.hot(limit=5):
+for submission in subreddit.hot(limit=20):
     for top_level_comment in submission.comments:
-        print(top_level_comment.body)
+        #print(top_level_comment.body)
         comments.append(top_level_comment.body)
         # print(type(comments[0]))
         if re.search("me too thanks", top_level_comment.body, re.IGNORECASE):
+            continue
             # top_level_comment.reply("Me 3 thx <3")
             # print top_level_comment.body
             # comments.append(tb(top_level_comment.body))
@@ -44,24 +45,36 @@ texts = []
 # loop through document list
 for i in comments:
 
+    print(i)
     # clean and tokenize document string
     raw = i.lower()
+
     tokens = tokenizer.tokenize(raw)
+    #print tokens
 
     # remove stop words from tokens
     stopped_tokens = [i for i in tokens if not i in en_stop]
+    #print stopped_tokens
 
     # stem tokens
     stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+    #print stemmed_tokens
 
     # add tokens to list
     texts.append(stemmed_tokens)
 
 # turn our tokenized documents into a id <-> term dictionary
 dictionary = corpora.Dictionary(texts)
+#print dictionary
 
 # convert tokenized documents into a document-term matrix
 corpus = [dictionary.doc2bow(text) for text in texts]
+#print corpus
+
+# generate LDA model
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=2, id2word = dictionary, passes=20)
+print(ldamodel.print_topics(num_topics=2, num_words=4))
+#print(ldamodel.print_topic(2,topn=4))
 
     # if re.search("me too thanks", submission.title, re.IGNORECASE):
     #     submission.reply("Size doesn't matter. It's about the passion.")
